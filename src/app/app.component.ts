3109ee5b697e10160db502b7753344cee5b76eb3
Ing Leandro Romero <lromero@rcnradio.com.co>
2017-09-06 12:40:11 -0500

    cambios nueva señal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   activar:boolean;
+  activarPapa:boolean;
+
   date:Date = new Date();
   dateactivo = new Date(2017,8,6,16).getTime();
+  datefinEvento = new Date(2017,8,6,18).getTime();
 
   constructor(){
-    if(this.date.getTime() <= this.dateactivo){
-      this.activar = true;
+    if(this.date.getTime() >= this.dateactivo){
+      
+      if(this.date.getTime() >= this.datefinEvento ){
+        this.activarPapa = true;
+        this.activar = false;
+      }else{
+        this.activar = true;
+        this.activarPapa = false;
+      }
     }else{
       this.activar = false;
+      this.activarPapa = false;
     }
   //console.log(this.dateactivo);
   }
