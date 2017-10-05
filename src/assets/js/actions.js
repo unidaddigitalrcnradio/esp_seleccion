a959ca4f7d6355da92573c8fbc442164b9d5527f
Leandro Romero <lromero@rcnradio.com>
2017-10-05 15:35:02 -0500

    a

--- src/assets/js/actions.js
+++ src/assets/js/actions.js
@@ -1,33 +1,9 @@
-// $(function () {
-//     var cont = document.getElementById('ppal')
-//     var header = document.getElementById('header');
-//     var headroom = new Headroom(header);
-//     headroom.init();
-//   });
+$(function () {
+    var header = document.getElementById('header');
+    var headroom = new Headroom(header);
+    headroom.init();
+  });
 
-(function() {
-    var header = new Headroom(document.querySelector("#header"), {
-        tolerance: 5,
-        offset : 205,
-        classes: {
-          initial: "animated",
-          pinned: "slideDown",
-          unpinned: "slideUp"
-        }
-    });
-    header.init();
-
-    var bttHeadroom = new Headroom(document.getElementById("btt"), {
-        tolerance : 0,
-        offset : 500,
-        classes : {
-            initial : "slide",
-            pinned : "slide--reset",
-            unpinned : "slide--down"
-        }
-    });
-    bttHeadroom.init();
-}());
 
 $(document).ready(function() {
 // Radio en vivo
